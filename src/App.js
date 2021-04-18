d434924f12d64ed94d444940d257feb143341c5f
mwyywm <hasanof.rasmus@gmail.com>
2021-04-18 23:49:04 +0200

    Made it so we actually have to press enter to show weather for the city

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 import WeatherBox from "./components/WeatherBox";
 import "./App.css";
 import axios from "axios";
-import { TextField } from "@material-ui/core";
 
 function App(props) {
+  const [searchCity, setSearchCity] = useState("");
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
 
   useEffect(() => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.REACT_APP_API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&APPID=${process.env.REACT_APP_API_KEY}`
       )
       .then((res) => {
         setWeather(res.data); // ???????????????????
@@ -20,17 +20,18 @@ function App(props) {
       .catch((err) => {
         console.log("err:", err);
       });
-  }, [city]);
+  }, [searchCity]);
 
   const handleInput = (e) => {
     setCity(e.target.value);
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`You submitted ${city}`);
-    console.log("weather:", weather);
-    console.log("apikey:", process.env.REACT_APP_API_KEY);
+    setSearchCity(city);
+    console.log(searchCity);
+    // console.log(`You submitted ${city}`);
+    // console.log("weather:", weather);
+    // console.log("apikey:", process.env.REACT_APP_API_KEY);
   };
 
   return (
@@ -43,7 +44,7 @@ function App(props) {
         )}
         <div className="formdiv">
           <form onSubmit={handleSubmit}>
-            <input type="input" onInput={handleInput} value={city} />
+            <input type="text" onInput={handleInput} value={city} />
           </form>
         </div>
       </div>
